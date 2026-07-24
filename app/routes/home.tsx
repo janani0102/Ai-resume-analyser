4119d35a40d3b7962f810affb6caa3bd534a5321
janani <jananisenthil1401@gmail.com>
2026-07-25 02:11:42 +0530

    second commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,20 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
+import type { Route} from "./+types/home";
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ResuMate" },
+    { name: "description", content: "Land more Interviews with me. I'll optimize your resume in seconds" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+<section className="main-section">
+  <div className="page-heading">
+    <h1>Track your Applications <br/>& Resume Ratings</h1>
+    <h2>Transform your resume with instant Ai-powered feedback</h2>
+  </div>
+
+</section>
+  </main>
 }
