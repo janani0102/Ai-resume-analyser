4ccff9c9162fcd0e9fbe2f3635c29d1b62ec4a24
janani <jananisenthil1401@gmail.com>
2026-07-26 00:29:31 +0530

    Authentication

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,12 @@
 
 import type { Route} from "./+types/home";
+import Navbar from "../components/Navbar";
+import ResumeCard from "../components/ResumeCard";
+import {resumes} from "../../constants";
+import {usePuterStore} from "~/lib/puter";
+import {useEffect,useState} from "react";
+import {Link, useNavigate} from "react-router";
+
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "ResuMate" },
@@ -8,13 +15,32 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-<section className="main-section">
-  <div className="page-heading">
-    <h1>Track your Applications <br/>& Resume Ratings</h1>
-    <h2>Transform your resume with instant Ai-powered feedback</h2>
-  </div>
+    const {auth}=usePuterStore();
+    const navigate = useNavigate();
 
-</section>
+    useEffect(()=>{
+      if(!auth.isAuthenticated) navigate("/auth?next=/");
+    },[auth.isAuthenticated])
+
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar/>
+
+    <section className="main-section  py-15">
+      <div className="page-heading">
+        <h1>Track your Applications <br/>& Resume Ratings</h1>
+        <h2>Transform your resume with instant Ai-powered feedback</h2>
+      </div>
+      {
+          resumes.length>0 && (
+              <div className="resumes-section">
+                {resumes.map((resume)=>(
+                    <ResumeCard key={resume.id} resume={resume} />
+
+
+                ))}
+              </div>
+          )}
+    </section>
+
   </main>
 }
